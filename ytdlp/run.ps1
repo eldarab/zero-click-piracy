9c339a7614df878ac3326d645393e44fa4bbd6de
Eldar Abraham <eldar.abraham@gmail.com>
2025-06-06 13:22:28 +0300

    ytdlp run

--- ytdlp/run.ps1
+++ ytdlp/run.ps1
@@ -1,1 +1,8 @@
-python -m yt_dlp -f bestvideo+bestaudio --merge-output-format mp4 -o "%(title)s.%(ext)s" https://vimeo.com/1084151360/e0220f8dfc
+$link = Read-Host "Paste the video link"
+$outputFolder = Join-Path $env:USERPROFILE "Videos"
+Write-Host "yt-dlp is executing from: $(where.exe yt-dlp)" -ForegroundColor Cyan
+$title = yt-dlp --get-filename -o "%(title)s" "$link"
+$filename = "$title.mp4"
+$fullPath = Join-Path $outputFolder $filename
+yt-dlp -f bestvideo+bestaudio --merge-output-format mp4 -o "$fullPath" "$link"
+Start-Process "explorer.exe" "/select,`"$fullPath`""
